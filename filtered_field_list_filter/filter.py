c1d63ffa886a9dbe86f70f746a16fe360b302e1b
unknown <tanno.sin@gmail.com>
2019-11-25 21:52:40 +0900

    field_pathに'__'が含まれる場合に対応

--- filtered_field_list_filter/filter.py
+++ filtered_field_list_filter/filter.py
@@ -5,11 +5,14 @@ from django.contrib.admin.utils import prepare_lookup_value
 def filtered_field_list_filter(*fields: str):
     class FilteredFieldListFilter(admin.RelatedFieldListFilter):
         def field_choices(self, field, request, model_admin):
+            filtered_field = self.field_path
             limit_choices_to = {
-                param.split("__", 1)[-1]: prepare_lookup_value(param, value)
+                param.replace(f"{filtered_field}__", "", 1): prepare_lookup_value(
+                    param, value
+                )
                 for (param, value) in request.GET.items()
                 if any(
-                    param.startswith(f"{self.field_path}__{field}") for field in fields
+                    param.startswith(f"{filtered_field}__{field}") for field in fields
                 )
             }
             return field.get_choices(
